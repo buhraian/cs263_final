536bc8a205a2d545191ce697f429899c3c85dc9d
Brian Li <brianli0317@gmail.com>
2024-03-13 16:30:35 -0700

    switching to linux desktop

---
+++ src/playground/hello_world/target/debug/deps/hello_world-ab765bef6c640697.d
@@ -0,0 +1,5 @@
+/Users/brianli/Documents/ucsb/cs263/cs263_final/playground/hello_world/target/debug/deps/hello_world-ab765bef6c640697: src/main.rs
+
+/Users/brianli/Documents/ucsb/cs263/cs263_final/playground/hello_world/target/debug/deps/hello_world-ab765bef6c640697.d: src/main.rs
+
+src/main.rs:
---
+++ src/playground/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/brianli/Documents/ucsb/cs263/cs263_final/playground/hello_world/target/debug/hello_world: /Users/brianli/Documents/ucsb/cs263/cs263_final/playground/hello_world/src/main.rs
